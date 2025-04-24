b74e1c3b8f6fb7272ea50999b8c1f5130731ac44
NatiaMinasiani <natiaminasiani@gmail.com>
2025-04-24 19:40:11 +0300

    fixed js problems

---
+++ src/search.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // ძებნა
+  const searchIcon = document.querySelector(".search-icon");
+  const searchText = document.querySelector(".search > div:first-child");
+  const searchInput = document.getElementById("searchInput");
+
+  function toggleSearch() {
+    if (searchInput.style.display === "none") {
+      searchInput.style.display = "inline-block";
+      searchInput.focus();
+    } else {
+      searchInput.style.display = "none";
+    }
+  }
+  function handleSearch(event) {
+    if (event.key === "Enter") {
+      const query = searchInput.value.trim();
+      if (query !== "") {
+        console.log("ძებნა:", query);
+        alert("ძებნა: " + query);
+      }
+    }
+  }
+  searchIcon.addEventListener("click", toggleSearch);
+  searchInput.addEventListener("keydown", handleSearch);
+});
